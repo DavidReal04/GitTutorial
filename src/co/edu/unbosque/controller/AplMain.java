10fe045d389ef58d2b73e5afd935151eeec4d1df
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-08-20 10:33:56 -0500

    New Function in AplMain

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -5,6 +5,7 @@ public class AplMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("La buena parce");
+		System.out.println("función en feature_3");
 	}
 
 }
